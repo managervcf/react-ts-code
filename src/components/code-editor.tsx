0ef62a25e51ffe6ef5e8ad113dff50489a09ded8
managervcf <managervcf@gmail.com>
2021-09-06 19:22:06 +0800

    Add optional fix for the JSX syntax highlighting.

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,11 @@
+import './code-editor.css';
+import './syntax.css';
 import { useRef } from 'react';
 import Editor, { EditorDidMount } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
+import codeShift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -23,6 +27,24 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
 
     // Change the tab size from 4 to 2.
     editor.getModel()?.updateOptions({ tabSize: 2 });
+
+    // Create a new instance of the highlighter.
+    const highlighter = new Highlighter(
+      // @ts-ignore
+      window.monaco,
+      codeShift,
+      editor
+    );
+
+    // If code changes inside the code editor, apply highlighting.
+    // Add weird options to prevent console.logging a syntax error
+    // with every keypress.
+    highlighter.highLightOnDidChangeModelContent(
+      () => {},
+      () => {},
+      undefined,
+      () => {}
+    );
   };
 
   // Formats the code inside the code editor.
@@ -31,22 +53,25 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     const unformattedCode = editorRef.current.getModel()?.getValue();
 
     // Format that value with configuration options for prettier.
-    const formattedCode = prettier.format(unformattedCode, {
-      parser: 'babel',
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      singleQuote: true,
-    });
+    const formattedCode = prettier
+      .format(unformattedCode, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      // Remove the last newline character from the formatted code.
+      .replace(/\n$/, '');
 
     // Set the formatted value vack in the editor.
     editorRef.current.setValue(formattedCode);
   };
 
   return (
-    <div>
+    <div className="editor-wrapper">
       <button
-        className="button button-format is-primary is-small"
+        className="button button-format is-primary is-small is-rounded"
         onClick={onFormatClick}
       >
         Format
